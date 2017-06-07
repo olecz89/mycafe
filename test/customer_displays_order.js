c91385eed636f81b8b2e67a28d52ee8ce10b8dbf
olecz89 <olek.czerwonka@gmail.com>
2017-06-07 22:55:20 +0200

    coding scenarios2
    some changes

--- test/customer_displays_order.js
+++ test/customer_displays_order.js
@@ -1,10 +1,23 @@
 "use strict";
 var chai = require("chai"),
     expect = chai.expect,
-    sinon = require("sinon");
+    sinon = require("sinon"),
+    orderSystemWith = require("../lib/orders");
+
 
 describe("Customer displays order", function() {
   context("Given that the order is empty", function() {
+    beforeEach(function() {
+      var orderDAO = {
+        byId: sinon.stub()
+      },
+          orderSystem = orderSystemWith(orderDAO);
+
+      orderDAO.byId..withArgs("some empty order id").returns([]);
+      
+      this.result = orderSystem.display("some empty order id");
+    });
+
     it("will show no order item");
     it("will show 0 as the total price");
     it("will only be possible to add a beverage");
