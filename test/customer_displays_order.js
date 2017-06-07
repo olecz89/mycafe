fef61432ed56290427dc6cd5a56e33023496a64a
olecz89 <olek.czerwonka@gmail.com>
2017-06-07 22:21:28 +0200

    first
    first

---
+++ test/customer_displays_order.js
@@ -0,0 +1,26 @@
+"use strict";
+var chai = require("chai"),
+    expect = chai.expect,
+    sinon = require("sinon");
+
+describe("Customer displays order", function() {
+  context("Given that the order is empty", function() {
+    it("will show no order item");
+    it("will show 0 as the total price");
+    it("will only be possible to add a beverage");
+  });
+
+  context("Given that the order contains beverages", function () {
+    it("will show one item per beverage");
+    it("will show the sum of beverages as total price");
+    it("will be possible to place the order");
+    it("will be possible to add a beverage");
+    it("will be possible to remove a beverage");
+    it("will be possible to change quantity of beverages");
+  });
+
+  context("Given that the order has pending messages", function() {
+    it("will show the pending messages");
+    it("there will be no more pending messages");
+  });
+});
